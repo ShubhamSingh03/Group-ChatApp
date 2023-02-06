b1d5adfad949e4d56cdf35bca121cad8052021e9
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-02-07 00:08:40 +0530

    components rendered with conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { auth } from "./auth/firebaseAuth";
+import { useAuthState } from "react-firebase-hooks/auth";
 import "./App.css";
 
+// components
+import Chat from "./components/chat/Chat";
+import SignIn from "./components/signIn/SignIn";
+
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
-    <div className="App">
-      <h1>Group Chat App</h1>
+    <div className="h-screen bg-appBg">
+      {user ? <Chat /> : <SignIn />}
+      <div className="text-xs fixed bottom-0 left-4 text-white font-semibold sm:text-sm">
+        Coded by:{" "}
+        <a href="https://shubhambhoj.in" target={"_blank"} rel="noreferrer">
+          Shubham Singh
+        </a>
+      </div>
     </div>
   );
 }
